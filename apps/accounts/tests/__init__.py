6f322118df96f9462c6e320443eeafc4f3ec299a
cjy <sapla@naver.com>
2026-02-07 21:04:26 +0900

    fix : accounts test_code with pytest
    list : views.py / models.py / forms.py

---
+++ apps/accounts/tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+- 테스트는 계산과 로직이 집중된 파일들 위주로 진행한다
+- models.py / views.py / utils.py / forms.py 등등
+"""
--- apps/transactions/apps.py
+++ apps/transactions/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TransactionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.transactions'  # ← 전체 경로로!
+
+    def ready(self):
+    # 이 부분이 없으면 signals.py는 그냥 장식일 뿐입니다.
+        import apps.accounts.signals
